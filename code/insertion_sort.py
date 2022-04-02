af612777cef13c2725f25e17de275a2328486c6d
Tim Harding <tim@timharding.co>
2022-04-02 13:44:09 -0700

    Insertion sort cleanup

--- code/insertion_sort.py
+++ code/insertion_sort.py
@@ -5,13 +5,11 @@ def main():
 
 
 def insertion_sort(array):
-    for j in range(len(array)):
-        key = array[j]
-        i = j - 1
-        while i >= 0 and array[i] > key:
-            array[i + 1] = array[i]
+    for (i, key) in enumerate(array):
+        while i > 0 and array[i-1] > key:
+            array[i] = array[i-1]
             i -= 1
-        array[i + 1] = key
+        array[i] = key
     return array
 
 
