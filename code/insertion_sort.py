7a12eb2afdbb05100fb1551a54e1424074bc943e
Tim Harding <tim@timharding.co>
2022-04-02 13:46:43 -0700

    Commenting insertion sort

--- code/insertion_sort.py
+++ code/insertion_sort.py
@@ -5,6 +5,10 @@ def main():
 
 
 def insertion_sort(array):
+    """This algorithm works by swapping larger elements
+    to the left until they are in the right position,
+    working from left to right on subsets of the array
+    until every element has been sorted."""
     for (i, key) in enumerate(array):
         while i > 0 and array[i-1] > key:
             array[i] = array[i-1]
